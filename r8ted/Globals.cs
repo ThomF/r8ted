6bc1f48e25abad97c555c89068bc63fe8a30ec8b
ThomF <seniorswanky@icloud.com>
2023-07-11 18:30:40 -0600

    init

---
+++ r8ted/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using r8ted.Repositories;
+global using r8ted.Services;
+global using r8ted.Models;
