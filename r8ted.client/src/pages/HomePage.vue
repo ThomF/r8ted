93e362aa2d772829dc60c965b2f9f12031787be6
ThomF <seniorswanky@icloud.com>
2023-07-15 23:26:15 -0600

    c

--- r8ted.client/src/pages/HomePage.vue
+++ r8ted.client/src/pages/HomePage.vue
@@ -4,7 +4,7 @@
       <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
         class="rounded-circle">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        welcome to critReels
+        welcome to MOVY
       </h1>
     </div>
   </div>
