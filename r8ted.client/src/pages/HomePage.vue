dca390b2ed496fe4f99dd450ae7d44b39f902d10
ThomF <seniorswanky@icloud.com>
2023-07-20 23:20:28 -0600

    c

--- r8ted.client/src/pages/HomePage.vue
+++ r8ted.client/src/pages/HomePage.vue
@@ -3,9 +3,8 @@
     <div class="home-card p-5 bg-white rounded elevation-3">
       <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
         class="rounded-circle"> -->
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center draggable-element" ref="draggableElement"
-        @mousedown="onMouseDown">
-        welcome to MVY
+      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
+        welcome to MVY!
       </h1>
     </div>
   </div>
@@ -14,41 +13,35 @@
 </template>
 
 <script>
-import interact from 'interactjs';
-
 export default {
-  mounted() {
-    interact(this.$refs.draggableElement)
-      .draggable({
-        listeners: {
-          move: this.onDragMove
-        }
-      });
-  },
-  methods: {
-    onMouseDown(event) {
-      // Prevent native dragging behavior
-      event.preventDefault();
-    },
-    onDragMove(event) {
-      const target = event.target;
-      const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-      const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      target.style.transform = `translate(${x}px, ${y}px)`;
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
+  setup() {
+
+    return {
+
     }
   }
-};
+}
 </script>
-  
-<style scoped>
-.draggable-element {
-  width: 450px;
-  height: 200px;
-  background-color: #f2f2f2;
-  border: 1px solid #ccc;
-  cursor: move;
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
 }
 </style>
