fd14746998377c0263722e1c506286ead8aabd18
ThomF <seniorswanky@icloud.com>
2023-07-31 15:39:41 -0600

    getting rating category INTs in the tables set

--- r8ted/dbSetup.sql
+++ r8ted/dbSetup.sql
@@ -13,6 +13,8 @@ CREATE TABLE IF NOT EXISTS review(
   title VARCHAR(255) NOT NULL,
   description VARCHAR(1000) NOT NULL,
   movie_id INT NOT NULL,
+  catOne INT,
+  catTwo INT,
   isPrivate BOOLEAN NOT NULL default false,
   FOREIGN KEY (user_id) REFERENCES accounts(id) ON DELETE CASCADE
 );
