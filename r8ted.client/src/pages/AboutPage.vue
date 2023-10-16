9aafcdfee6773ac148cf717c2dff4dc015fd5652
ThomF <seniorswanky@icloud.com>
2023-10-16 14:26:31 -0600

    c

--- r8ted.client/src/pages/AboutPage.vue
+++ r8ted.client/src/pages/AboutPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is the about page, it should host various QnAs about mvy and the ideas of the site</h1>
+    <h2>Maybe an about the team?</h2>
   </div>
 </template>
 
